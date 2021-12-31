e53c8ad7585d25832ecb06f105f38cbd1ffdd2e1
lim <wamidulim@gmail.com>
2021-12-31 17:47:52 +0800

    new year count down

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-## Ber months Countdown
+## New Year Countdown
 
-Landing page that counts down from the current date to the bermonths (September 1)
+Landing page that counts down from the current date to the next new year
 
 ## Project Specifications
 
 - Create landing page with HTML/CSS
 - Calculate the days, hours, mins and seconds to the new year
 - Insert values into the DOM
-- Show a spinner right before loading the countdown 
-
-
-### demo link
-
-- https://ber-months-countdown.netlify.app/
-
-
-##### Why does Christmas in the Philippines start in September? 
-https://www.rappler.com/brandrap/lifestyle-and-entertainment/shakeys-ber-bundle-christmas
-
-
-- song credits to https://www.youtube.com/watch?v=wBoyzbS7PQI
+- Show a spinner right before loading the countdown
+- Show the coming year in the background
--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
 const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
-const time = new Date(`September 01 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
 
 // Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
-  const diff = time - currentTime;
+  const diff = newYearTime - currentTime;
 
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
@@ -35,8 +38,3 @@ setTimeout(() => {
 
 // Run every second
 setInterval(updateCountdown, 1000);
-
-// to play audio
-window.addEventListener('click', () => {
-  document.querySelector('#audio').play();
-});
